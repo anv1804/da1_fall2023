9230f2a5f30dd3bff73ee564284823826e52514d
Nguyễn An <annguyen04@hotmail.com>
2023-11-20 23:25:48 +0700

    getid user

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+
 include './model/pdo.php';
 include './model/account.php';
 include './model/hotels.php';
 include './model/rooms.php';
+include './model/users.php';
+
 
 include './user/layout/header.php';
 include './user/layout/footer.php';
