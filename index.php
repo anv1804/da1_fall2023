ecd805bedd34f76d4bab739134a6c34ba1881b39
Nguyễn An <annguyen04@hotmail.com>
2023-12-11 20:10:57 +0700

    20h

--- index.php
+++ index.php
@@ -1,18 +1,27 @@
 <?php
-    include './model/pdo.php';
-    include './model/account.php';
-    include './model/hotels.php';
-    include './model/rooms.php';
-    include './model/users.php';
+include './model/pdo.php';
+include './model/account.php';
+include './model/hotels.php';
+include './model/rooms.php';
+include './model/users.php';
 if (isset($_SESSION['user'])) {
     $user = $_SESSION['user'];
 } else if (isset($_COOKIE['user'])) {
     $user = json_decode($_COOKIE['user'], true);
 }
-if (isset($user) && $user['user_role'] == 1) {
+if ($user && $user['user_role'] == 1) {
     header('Location: admin.php');
 } else {
-    include './user/layout/header.php';
-    include './user/layout/footer.php';
+    if (isset($user) && $user['user_role'] == 0) {
+        $userEmail = $user['user_email'];
+        $result = user($userEmail);
+        $userID = $result[0]['user_id'];
+        include './user/layout/header.php';
+        include './user/layout/footer.php';
+    } else {
+        include './user/layout/header.php';
+        include './user/layout/footer.php';
+    }
+
 }
 ?>
